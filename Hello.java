2aa09e209ee77671de6cb0afb4be6d11cc22c587
Synchoresis <46752757+Synchoresis@users.noreply.github.com>
2019-01-16 10:35:34 -0500

    Java Hello from lab 1

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/************************************************************
+*Java Program Name
+*************************************************************
+*Project/Program description
+*_____________________________________________________________
+*1/16/19
+*CMSC 255 3
+*************************************************************/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Your Name");
+		System.out.println("Java Project Name");
+		System.out.println("Version Date");
+		System.out.println("Course Number and Section");
+		System.out.println();
+	}
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
